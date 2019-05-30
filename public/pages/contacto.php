f0c91642c67a28c97dc021faed372714184aed08
david cornejo <lcornejob@est.ups.edu.ec>
2019-05-29 20:16:27 -0500

    Create contacto.php
    estructura para envio de correos al administrador

---
+++ public/pages/contacto.php
@@ -0,0 +1,62 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){
+        header("Location: /PracticaExamen/public/vista/login.html");
+    }
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"> 
+		<title>Contactanos</title>
+		<!-- ESTILOS -->
+        <link href="../estilos/estilos2.css" rel="stylesheet" />
+	</head>
+	
+	<body>
+		<?php $codigo = $_GET['codigo']; ?>
+		
+		<h1>CONTÁCTANOS</h1>
+		<form method="post" action="pagina2.php">
+		<input type="hidden" id="codigo" name="codigo" value="<?php echo $codigo ?>">
+		<table id="contacto" border align="center">
+			
+			<tr>
+				<td>From:</td>
+				<td colspan="2"><textarea name="remite" id="remite" rows="1" cols="100" id="textview" type="text" value="<?php echo buscarCorreo($codigo) ?>" ></textarea></td>
+			</tr>
+			
+			<tr>
+				<td>Asunto:</td>
+				<td colspan="2"><textarea name="asunto" id="asunto" rows="2" cols="100" id="textview" type="text"></textarea></td>
+			</tr>
+							
+			<tr>
+				<td colspan="2"><textarea name="mensaje" id="mensaje" rows="10" cols="114" id="textview" type="text"></textarea></td>
+			</tr>
+			
+			<tfoot>
+				<tr>
+					<td align="center"><input id="button" type="submit" value="ENVIAR"></td>
+					<td align="center"><input id="button" type="button" value="CANCELAR"></td>
+					
+				</tr> 
+			</tfoot>	
+		</table>
+		</form>
+	</body>
+</html>
+
+<?php
+    function buscarCorreo($codigoCorreo){
+        include '../../config/conexionBD.php';
+        $sql1 = "SELECT usu_correo FROM usuarios WHERE usu_codigo='$codigoCorreo'";
+        $result = $conn->query($sql1);
+        if($result->num_rows > 0){
+            while($row = $result->fetch_assoc()){
+                $direccionCorreo=$row["usu_correo"];
+            }
+        }
+        return $direccionCorreo;
+    }
+?>
