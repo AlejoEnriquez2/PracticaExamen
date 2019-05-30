fde25309f70795ebd0e3da61af373d5b2041c07a
david cornejo <lcornejob@est.ups.edu.ec>
2019-05-30 16:13:17 -0500

    Update index.php

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -1,4 +1,9 @@
-
+<?php
+    session_start();
+    if(!isset($_SESSION['isLogged']) || $_SESSION['isLogged'] === FALSE){
+        header("Location: PracticaExamen/public/vista/login.html");
+    }
+?>
 <!DOCTYPE html>
 <html>
     <?php
