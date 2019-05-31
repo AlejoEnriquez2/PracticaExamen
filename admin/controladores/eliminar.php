68f7b1af4fa29949f0eebffb0e3f72d0d952507e
david cornejo <lcornejob@est.ups.edu.ec>
2019-05-31 12:53:55 -0500

    Update eliminar.php

--- admin/controladores/eliminar.php
+++ admin/controladores/eliminar.php
@@ -3,27 +3,26 @@
     <head>
         <meta charset="UTF-8">
         <title>Eliminar datos de persona</title>
-        <link type="text/css" rel="stylesheet" href="../../../style.css">
     </head>
     <body>
         <form class="box">
         <?php
 
             //Incluir conexion a la BD
-            include '../../../config/conexionBD.php';
+            include '../../config/conexionBD.php';
             $codigo_admin = $_GET["codigo_admin"];
             $codigo = $_POST["codigo"];
 
             date_default_timezone_set("America/Guayaquil");
             $fecha = date("Y-m-d H:i:s",time());
-            $sql = "UPDATE usuario SET usu_eliminado = 'S', usu_fecha_modificacion = '$fecha' WHERE usu_codigo = '$codigo'";
+            $sql = "UPDATE usuarios SET usu_eliminado = 'S', usu_fecha_modificacion = '$fecha' WHERE usu_codigo = '$codigo'";
 
             if ($conn->query($sql) == TRUE){
                 echo "<p>Se ha eliminado los datos correctamente</p>";
             }else{
                 echo "<p>Error".$sql."<br>".mysqli_error($conn)."</p>";
             }
-            echo "<a href='../../vista/admin/usuarios.php?codigo_admin=".$codigo_admin."'>Regresar</a>";
+            echo "<a href='/PracticaExamen/admin/vista/index.php?codigo_admin=".$codigo_admin."'>Regresar</a>";
 
             $conn->close();
         ?>
