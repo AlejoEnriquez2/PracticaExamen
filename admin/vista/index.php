84c6ea20e424204a561f8403bd322fa45956d870
david cornejo <lcornejob@est.ups.edu.ec>
2019-05-31 02:21:26 -0500

    Update index.php

--- admin/vista/index.php
+++ admin/vista/index.php
@@ -102,5 +102,44 @@ function buscarCorreo($codigoCorreo){
 $conn->close();
 ?>
 </table>
+	
+<h3>Juegos Registrados</h3>
+	
+<table style="width:100%">
+    <tr>
+        <th>Titulo</th>
+        <th>Descripcion</th>
+        <th>Imagen</th>
+        <th>Precio</th>
+        <th>Fecha</th>
+    </tr>
+     
+     
+ <?php
+ include '../../config/conexionBD.php';
+ $sql = "SELECT * FROM juegos";
+ $result = $conn->query($sql);
+
+ if ($result->num_rows > 0) {
+
+while($row = $result->fetch_assoc()) {
+ echo "<tr>";
+ echo " <td>" . $row["jue_nombre"] . "</td>";
+ echo " <td>" . $row['jue_descripcion'] ."</td>";
+ echo " <td>" . $row['jue_imagen'] . "</td>";
+ echo " <td>" . $row['jue_precio'] . "</td>";
+ echo " <td>" . $row['jue_fecha'] . "</td>";
+ echo " <td> <a href='eliminar.php?codigo=" . $row['usu_codigo'] . "'>Eliminar</a> </td>";
+ echo " <td> <a href='modificar.php?codigo=" . $row['usu_codigo'] . "'>Modificar</a> </td>";
+ echo "</tr>";
+}
+    } else {
+    echo "<tr>";
+    echo " <td colspan='7'> No existen juegos registradas en el sistema </td>";
+    echo "</tr>";
+    }
+ $conn->close();
+ ?>
+ </table>
 </body>
 </html>
